b46648eae72c4c4b30a9b72aa99d903d3afab11f
h-mkrtchyan <hakobmkrtchyann@gmail.com>
2016-08-29 22:09:25 +0400

    actions

--- AutomatedTellerMachine/AutomatedTellerMachine/App_Start/RouteConfig.cs
+++ AutomatedTellerMachine/AutomatedTellerMachine/App_Start/RouteConfig.cs
@@ -14,10 +14,25 @@ namespace AutomatedTellerMachine
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Serial number",
+                url: "serial/{letterCase}",
+                defaults: new
+                {
+                    controller = "Home", action = "Serial", letterCase = "upper"
+                }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index",
+                    id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "News",
+                url: "news/{topNews}",
+                defaults: new { controller = "Home", action = "TopNews", topNews = "topNews" });
         }
     }
 }
